02a9a4840526bf04f7395845a3b01da0594dc235
Crashmet <shoma.mtv@gmail.com>
2023-04-17 02:33:35 +0300

    version 1.1.1

--- src/App.vue
+++ src/App.vue
@@ -131,7 +131,7 @@
                       id="username"
                       autocomplete="..."
                       class="mr-4 w-32 rounded block ring-1 ring-inset ring-gray-300 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 sm:text-sm hover:bg-gray-50"
-                      placeholder="Введите текст..."
+                      placeholder=" Print"
                       v-model="inputTextImg"
                     />
                   </div>
@@ -167,7 +167,7 @@
                       class="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
                       id="menu-button"
                     >
-                      Коли-во полигонов
+                      {{ polygonPointsValue }}
                       <svg
                         class="-mr-1 h-5 w-5 text-gray-400"
                         viewBox="0 0 20 20"
